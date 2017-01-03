69277cb9185671521af14b881c9ba93d2301302b
Ziyao Zhang <ziyaointl@gmail.com>
2017-01-03 22:18:41 +0800

    Fixed footer displacement on category.php
    1. Removed main tags
    2. Updated css

--- Chinese/category.php
+++ Chinese/category.php
@@ -1,14 +1,12 @@
 <?php get_header(); ?>
 
-<main role="main">
-    <div class="container-fluid catalog">
-        <!-- section -->
-        <h1 class="catalog-title"><?php single_cat_title() ?></h1>
+<div class="container-fluid catalog">
+    <!-- section -->
+    <h1 class="catalog-title"><?php single_cat_title() ?></h1>
 
-        <?php get_template_part('loop'); ?>
+    <?php get_template_part('loop'); ?>
 
-    </div>
-    <!-- /section -->
-</main>
+</div>
+<!-- /section -->
 
 <?php get_footer(); ?>
